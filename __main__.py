ceb9c6467244ac76546cc6161735b81f149f85b8
Saifallah Omar (STUDENT) <s298757@student.salfordcc.ac.uk>
2022-02-08 14:53:52 +0000

    qahzvqzmn

--- __main__.py
+++ __main__.py
@@ -1,5 +1,44 @@
+from re import S
+
+
 class Board():
+    
+    def __init__(self):
+        self.board = [
+            [".",".",".",".",".","."],
+            [".",".",".",".",".","."],
+            [".",".",".",".",".","."],
+            [".",".",".",".",".","."],
+            [".",".",".",".",".","."],
+            [".",".",".",".",".","."]
+            ]
+        
+        
+    """
+        Hello, this is my function defininton
+    """
+    def print(self):
 
-    def __init__(self) -> None:
-        print("helolo")
+        em = ""
+        for y in self.board:
+            for x in y:
+                em += x + " "
+            em += "\n"
+        print(em)
+    
+    def dropD(self, col):
+        # self.board[0][0] = "X"
+        row = 5
+        while self.board[row][col] != ".":
+            row -= 1
+        
+        self.board[row][col] = "X"
+        
+        
 
+
+myBoard = Board()
+myBoard.dropD(0)
+myBoard.print()
+
+myboard2 = Board()
